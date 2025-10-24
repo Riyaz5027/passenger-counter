668534742deea7a8aed58c393095709e5c6c2b91
Riyaz <riyazshaik5027@gmail.com>
2025-10-24 18:20:39 +0530

    First javascript using let keyword, console.log, function keyword

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.getElementById("count-el").innerText=5050;
+//document.getElementById("count-el").innerText=5050;
+//let count=30;
+//console.log(count);
+
+
+let counter=0
+let countEl=document.getElementById("count-el")
+console.log(countEl)
+function increment()
+{
+    counter=counter+1
+    countEl.innerText=counter
+}
